7017a35317dc23c0687bee5ae01a8b2eccd48edd
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-04-02 11:28:27 +0200

    test for ArrowLeft

--- index.js
+++ index.js
@@ -3,21 +3,17 @@
 
 const dodger = document.getElementById("dodger");
 
-dodger.style.backgroundColor = "#FF69B4";
+function moveDodgerLeft() {
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
 
-dodger.style.bottom = "0px";
-
-dodger.style.left = "0px";
-
-document.addEventListener("keydown", function (event) {
-    console.log(event);
-  });
-
-document.addEventListener("keydown", function (event) {
-    if (event.key === "ArrowLeft") {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-
+if (left > 0) {
     dodger.style.left = `${left - 1}px`;
+}
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
     }
-});
+  });
