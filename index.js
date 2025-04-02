bdc0e38020bbee94a8ebac246fcd17ce85fb3642
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-04-02 11:20:24 +0200

    eventListener

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+"use strict"
+
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+
+dodger.style.left = "0px";
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+  });
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    dodger.style.left = `${left - 1}px`;
+    }
+});
